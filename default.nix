81f9e1c53179adf414d39ca8f7dedc1a9c291b02
Vladyslav Mykhailichenko <dywedir@pm.me>
2018-08-13 10:20:37 +0000

    Move packages to pkgs subdir

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 with { inherit (pkgs) callPackage; };
 
 {
-  iosevka-comp-lig = callPackage ./iosevka-comp-lig {};
+  iosevka-comp-lig = callPackage ./pkgs/iosevka-comp-lig {};
 
-  pianoteq-stage = callPackage ./pianoteq-stage { };
+  pianoteq-stage = callPackage ./pkgs/pianoteq-stage { };
 }
